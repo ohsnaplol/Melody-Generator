76e468bfef4515b368e46c4b8d6a87b6a59bf39e
cgavidia <cagavidia@hotmail.com>
2016-11-06 14:07:38 -0500

    Update MG_MainTest.java

--- MelodyGenerator/src/main/MG_MainTest.java
+++ MelodyGenerator/src/main/MG_MainTest.java
@@ -24,22 +24,44 @@ public class MG_MainTest {
 	
 	@Parameters
 	public static Collection<Object[]> data() {
-		return Arrays.asList(new Object[][] {        	
-        	{"Db Eb F Gb Ab Bb C", "10", "127"},
-        	{"g# a# b c# d# e# f##", "30", "80"},
-        	{"A B C D E F ", "7", "120"},
-        	{"Db Eb F Gb Ab Bb C ", "", "110"},
-        	{"A B C D E F G", "9", ""},
-        	//{"","6", "110"},
+		return Arrays.asList(new Object[][] { 
+			
+			//Valid inputs
+			{"10","170","20","true"},	//[0]
+			{"8","150","0","true"},		//[1]
+			{"6","110","10","true"},	//[2]	
+			{"15","90","32","true"},	//[3]	
+			{"20","200","50","true"},	//[4]
+			{"1","1","12","true"},		//[5]
+			{"1","96","30","true"},		//[6]
+			{"3","1","47","true"},		//[7]
+			
+			//Invalid inputs
+			{"\n0","0","23","true"},	//[8]
+			{"0","125","14","true"},	//[9]
+			{"9","0","50","false"},		//[10]
+			{"3wh","110","10","true"},	//[11]
+			{"6","3wh","6","true"},		//[12]
+			{"11","70","3wh","true"},	//[13]
+			{"","200","5","true"},		//[14]
+			{"5","","12","true"},		//[15]
+			{"12","160","","true"},		//[16]
+			{"900","250","29","false"},	//[17]
+			{"10","900","2","false"},	//[18]
+			{"11","130","52","true"},	//[19]
+			
+			
         });	
 	}
 
 	@Parameter
-	public String key;
-	@Parameter(value=1)
 	public String numOfNotes;
-	@Parameter(value=2)
+	@Parameter(value=1)
 	public String tempo;
+	@Parameter(value=2)
+	public String keyIndex;
+	@Parameter(value=3)
+	public String playSound;
 	
 	
 	
@@ -66,26 +88,27 @@ public class MG_MainTest {
 
 	@Test
 	public void test() {
-		//Pattern pattern = new Pattern();
-		
-		Player player = new Player();
-		
+
 		StringBuilder sb = new StringBuilder();
 		sb.append(numOfNotes);
 		sb.append(" ");
 		sb.append(tempo);
+		sb.append(" ");
+		sb.append(keyIndex);
+		sb.append(" ");
+		sb.append(playSound);
 		System.out.println(sb.toString());
+		
+		MG_Main mainTest = new MG_Main();
+		
+		String[] myArguments = new String[4];
+		myArguments[0] = numOfNotes;
+		myArguments[1] = tempo;
+		myArguments[2] = keyIndex;
+		myArguments[3] = playSound;
+			
+		mainTest.main(myArguments);
 
-		Song.numOfNotes = Integer.parseInt(numOfNotes);
-		Song.tempo = Integer.parseInt(tempo);
-		
-		player.play(key,sb.toString());
-		System.out.println(key);
-	
-		//assertEquals("",pExpect,calc.calculate(sb.toString()));
-		//System.out.println(calc.calculate(sb.toString()));
-		
-		//fail("Not yet implemented");
 	}
 
 }
